81b518b959e1468f355c4c2f95926d398a9b6052
Eric Morales <ericdaniel3103@gmail.com>
2022-01-14 09:54:49 -0600

    State a useSelectMoneda y retornando valor

--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import styled from "@emotion/styled";
 
 const Label = styled.label`
@@ -19,11 +19,13 @@ const Select = styled.select`
 `;
 
 const useSelectMonedas = (label, opciones) => {
+  const [state, setState] = useState("");
+
   const SelectMonedas = () => (
     <>
       <Label htmlFor="">{label}</Label>
 
-      <Select>
+      <Select value={state} onChange={(e) => setState(e.target.value)}>
         <option value="">-- Seleccione --</option>
         {opciones.map((opcion) => (
           <option key={opcion.id} value={opcion.id}>
@@ -34,7 +36,7 @@ const useSelectMonedas = (label, opciones) => {
     </>
   );
 
-  return [SelectMonedas];
+  return [state, SelectMonedas];
 };
 
 export default useSelectMonedas;
