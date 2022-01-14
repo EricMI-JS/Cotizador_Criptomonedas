afc4f5a72477c3187f654d532096b42c2840fd8d
Eric Morales <ericdaniel3103@gmail.com>
2022-01-14 10:08:55 -0600

    Consultando API Criptomonedas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect } from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from "../data/monedas";
@@ -23,6 +23,16 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+    };
+    consultarAPI();
+  }, []);
 
   return (
     <form>
