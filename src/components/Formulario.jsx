aa46aafef89f713c0751f4ae25f72fc03380c47a
Eric Morales <ericdaniel3103@gmail.com>
2022-01-14 09:35:08 -0600

    Creando Custom Hook useSelectMonedas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
 
 const InputSubmit = styled.input`
   background-color: #9497ff;
@@ -19,8 +20,12 @@ const InputSubmit = styled.input`
 `;
 
 const Formulario = () => {
+  const [SelectMonedas] = useSelectMonedas("Elige tu Moneda");
+
   return (
     <form>
+      <SelectMonedas />
+
       <InputSubmit type="submit" value="Cotizar" />
     </form>
   );
