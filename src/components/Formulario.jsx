df9dcfe868ab6fc70c8b4368045af911cc71f3a6
Eric Morales <ericdaniel3103@gmail.com>
2022-01-14 10:35:36 -0600

    Validando el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import styled from "@emotion/styled";
+import Error from "./Error";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import { monedas } from "../data/monedas";
 
@@ -23,6 +24,7 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
   const [criptos, setCriptos] = useState([]);
+  const [error, setError] = useState(false);
 
   const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
   const [criptomoneda, SelectCriptoMoneda] = useSelectMonedas(
@@ -49,13 +51,27 @@ const Formulario = () => {
     consultarAPI();
   }, []);
 
-  return (
-    <form>
-      <SelectMonedas />
-      <SelectCriptoMoneda />
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-      <InputSubmit type="submit" value="Cotizar" />
-    </form>
+    if ([moneda, criptomoneda].includes("")) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+  };
+
+  return (
+    <>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptoMoneda />
+
+        <InputSubmit type="submit" value="Cotizar" />
+      </form>
+    </>
   );
 };
 
